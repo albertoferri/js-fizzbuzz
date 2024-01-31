9a859ac1755310f49bcf0f21a0031ab9f5fcef84
albertoferri <falberto99@gmail.com>
2024-01-31 15:28:08 +0100

    aggiunte classi ai quadrati multipli di 5 e 3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // creo una griglia con dei numeri che vanno da 0 a 100
 const grillElement = document.querySelector("#griglia");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
+    grillElement.style.fontWeight = "bold"
+    
+    if (i % 5 == 0) {
+        grillElement.innerHTML += `<div class="square buzz">${i}</div>`;
+    } else if (i % 3 == 0) {
+        grillElement.innerHTML += `<div class="square fizz">${i}</div>`;
+    } else {
+        grillElement.innerHTML += `<div class="square">${i}</div>`;
+    }
+    
 
-    grillElement.innerHTML += `<div class="square" >${i}</div>`
-
-    // console.log(grillElement)
 }
